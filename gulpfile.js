c15301601f7ffd06cc620fd6cfa87448fbf961c0
matheusggds <matheusggds@hotmail.com>
2017-09-12 18:16:39 -0300

    gulp sw-precache

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var swPrecache = require('sw-precache');
+
+gulp.task('generate-sw', function() {
+  var swOptions = {
+    staticFileGlobs: [
+      './index.html',
+      './images/*.{png,svg,gif,jpg}',
+      './scripts/*.js',
+      './styles/*.css'
+    ],
+    stripPrefix: '.',
+    runtimeCaching: [{
+      urlPattern: /^https:\/\/publicdata-weather\.firebaseio\.com/,
+      handler: 'networkFirst',
+      options: {
+        cache: {
+          name: 'weatherData-v3'
+        }
+      }
+    }]
+  };
+  return swPrecache.write('./service-worker.js', swOptions);
+});
